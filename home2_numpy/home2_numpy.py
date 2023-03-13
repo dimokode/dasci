e3abbe2f310e514d49aa9015396959515761694b
dimokode <dok0032m@gmail.com>
2023-03-13 18:06:38 +0300

    home2 numpy

--- home2_numpy/home2_numpy.py
+++ home2_numpy/home2_numpy.py
@@ -22,6 +22,34 @@ A = np.array(
 
 # print(cumsum(A))
 
+def np_transformation(X, a=1):
+    """  
+    X: np.array[num_row, num_column]          --- матрица-аргумент
+    a: float                                  --- значение для преобразования нечетных элементов строк в X
+    return S: np.array[num_row, num_column*2] --- матрица, где строки являются 
+    сконкатенированными строками изначальной матрицы X со строками, являющимися их преобразованиями
+
+    Функция принимает на вход матрицу X размерностью n x m, число a и 
+    возвращает  матрицу с размерностью n x m*2, i-ая строчка которой является склеенной
+    i-ой строкой X с ее преобразованием ее строки transformation(X[i]), записанном в обратном порядке, 
+    где преобразование для числа k определено как:
+    transformation(k) = a if ind(k) % 2 == 0 else k**3
+
+    В реализации этой функции необходимо использовать функционал пакета numpy
+
+    """ 
+    X1 = X.copy()
+    new_X = np.zeros((X.shape[0], X.shape[1]*2))
+    for i in range(new_X.shape[0]):
+        new_X[i][:X.shape[1]] = X[i]
+        X1[i][1:X.shape[1]:2] = np.ones(len( X1[i][1:X.shape[1]:2]))*a
+        X1[i][0:X.shape[1]:2] = pow(X1[i][0:X.shape[1]:2],3) 
+        
+        new_X[i][X.shape[1]:] = X1[i][::-1]
+    
+    return new_X
+
+
 
 def transform(X, a=1):
     X1 = X.copy()
@@ -40,13 +68,35 @@ def transform(X, a=1):
         ss = np.concatenate((X[i], X1[i]), axis=0)
         print(type(ss))
         new_X.append(ss)
-        new_X = np.asarray(new_X)
+    new_X = np.asarray(new_X)
         
     
     print(type(new_X))
     return new_X
 
-X = np.array([[100, 200, 300, 400, 500]])
+X = np.array([[100, 200, 300, 400, 500], [100, 200, 300, 400, 500]], dtype='int32')
 
 print(transform(X))
+print()
+print(np_transformation(X))
 print(X)
+
+
+def encode(a):
+    el = np.array([a[0]])
+    n = np.array([1])
+    j = 0
+    for i in a[1:]:
+        if i ==  el[j]:
+            n[j] += 1
+        else:
+            el = np.append(el, i)
+            n = np.append(n, 1)
+            j += 1
+            
+                   
+    return el, n
+
+
+a = np.array([1, 2, 2, 3, 3, 1, 1, 5, 5, 2, 3, 3])
+print(encode(a))
